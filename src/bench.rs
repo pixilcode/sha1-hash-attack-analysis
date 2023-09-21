1e1f8e4a18a1b60f6f14d022d2af58e93ffa5937
Brendon Bown <brendon.bown@gmail.com>
2023-09-21 15:28:50 -0600

    Add display ability to BenchResult

--- src/bench.rs
+++ src/bench.rs
@@ -1,29 +1,33 @@
 use rayon::prelude::*;
+use std::fmt;
 
 use crate::{attack::{collision_attack, preimage_attack}, hash::sha1_hash};
 
 const BIT_SIZES: [usize; 6] = [8, 11, 16, 22, 27, 32];
 
-const DEFAULT_SAMPLES: usize = 50;
-
+#[derive(Debug)]
 pub struct BenchResult(usize);
 
-pub fn run_collision_bench(bit_size: usize, num_samples: Option<usize>) -> Vec<BenchResult> {
-	let num_samples = num_samples.unwrap_or(DEFAULT_SAMPLES);
-	(0..num_samples).into_par_iter().map(|_| {
+impl fmt::Display for BenchResult {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		self.0.fmt(f)
+    }
+}
+
+pub fn run_collision_bench(bit_size: usize, num_samples: usize) -> Vec<BenchResult> {
+	(0..num_samples).into_iter().map(|i| {
 		let guess_start_value = rand::random();
-		let result = collision_attack(guess_start_value, bit_size);
-		BenchResult(result)
+				let result = collision_attack(guess_start_value, bit_size);
+				BenchResult(result)
 	}).collect()
 }
 
-pub fn run_preimage_bench(bit_size: usize, num_samples: Option<usize>) -> Vec<BenchResult> {
-	let num_samples = num_samples.unwrap_or(DEFAULT_SAMPLES);
-	(0..num_samples).into_par_iter().map(|_| {
+pub fn run_preimage_bench(bit_size: usize, num_samples: usize) -> Vec<BenchResult> {
+	(0..num_samples).into_par_iter().map(|i| {
 		let preimage: [char; 16] = rand::random();
 		let preimage: String = String::from_iter(preimage);
 		let preimage_hash = sha1_hash(preimage, bit_size);
 		let result = preimage_attack(preimage_hash, bit_size);
-		BenchResult(result)
+				BenchResult(result)
 	}).collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,9 @@ mod bench;
 mod hash;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Beginning!");
+    let result = bench::run_collision_bench(8, 1);
+    println!("Printing results:");
+    println!("{:?}", result);
+    println!("Done!");
 }
